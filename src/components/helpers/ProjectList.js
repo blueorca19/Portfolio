327a61ed28559a73651cd865867eb2f25584b8bb
Mona Lisa <149578803+blueorca19@users.noreply.github.com>
2024-08-05 16:57:38 +0200

    update

--- src/components/helpers/ProjectList.js
+++ src/components/helpers/ProjectList.js
@@ -2,6 +2,8 @@ import project01 from "./../../img/projects/01.jpg";
 import project01Big from "./../../img/projects/01-big.jpg";
 import project03 from "./../../img/projects/03-big.png";
 import project03Big from "./../../img/projects/03-big.png";
+import project02 from "./../../img/projects/02-big.png";
+import project02Big from "./../../img/projects/02-big.png";
 
 const projects = [
     {
@@ -11,7 +13,6 @@ const projects = [
         imgBig: project01Big,
         siteLink: "https://project-wishlist-giftlistify-ccqrj.ondigitalocean.app/"
     }, 
-    
 
     {
         title: 'Portfolio',
@@ -19,6 +20,14 @@ const projects = [
         img: project03,
         imgBig: project03Big,
         siteLink: "https://blueorca19.github.io/Portfolio/"
+    },
+
+    {
+        title: 'Salon Master',
+        skills: 'SpringBoot, REST API, JPA, Hibernate, Swagger, Gradle',
+        img: project02,
+        imgBig: project02Big,
+        siteLink: "https://dbdiagram.io/d/SalonMaster-6676f2cf5a764b3c72255b54"
     }
 ];
 export {projects};
